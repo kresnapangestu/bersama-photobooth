6074869e9c2234dc5a9268eb5f246b96f658d351
Mohammad Iqbal Kresna Pangestu <mohammadiqbalkresna@gmail.com>
2025-04-17 21:34:49 +0700

    changing photocapture code

--- src/app/photo-capture/page.tsx
+++ src/app/photo-capture/page.tsx
@@ -1,11 +1,20 @@
-"use client";
-import React from "react";
+import React, { Suspense } from "react";
 import { useSearchParams } from "next/navigation";
 import PhotoCaptureTemplate from "@/components/templates/PhotoCaptureTemplate";
 
 export default function PhotoCapturePage() {
+  return (
+    <Suspense fallback={<p>Loading...</p>}>
+      <SearchParamsContent />
+    </Suspense>
+  );
+}
+
+const SearchParamsContent = () => {
   const searchParams = useSearchParams();
   const frame = searchParams.get("frame");
+
   if (!frame) return <p className="p-6">No frame selected.</p>;
+
   return <PhotoCaptureTemplate frame={frame} />;
-}
+};
