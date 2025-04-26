9c51e44069db95f3bccbda4ee75f0d0d19552ee6
Mohammad Iqbal Kresna Pangestu <mohammadiqbalkresna@gmail.com>
2025-04-26 12:24:32 +0700

    fixing unescaped entities when deployed

--- src/components/templates/AboutTemplate.tsx
+++ src/components/templates/AboutTemplate.tsx
@@ -5,13 +5,14 @@ export default function AboutTemplate() {
     <div className="p-6 flex flex-col items-center gap-6 font-semibold h-[80vh] justify-center">
       <span className="text-[50px]">About</span>
       <span className="w-[50vw] text-xl text-center">
-        Ber-sama is more than just for those who chase the results—it's for the
-        dreamers, the explorers, and those who want to create magical memories
-        that last forever! Whether you're looking to capture spontaneous
-        moments, celebrate life, or make every day a little brighter, we&apos;re
-        here to turn your vision into reality. It&apos;s all about bringing joy,
-        embracing the fun, and never being just another face in the crowd.
-        Let&apos;s make your memories as magical as the moments themselves!
+        Ber-sama is more than just for those who chase the results—it&apos;s for
+        the dreamers, the explorers, and those who want to create magical
+        memories that last forever! Whether you&apos;re looking to capture
+        spontaneous moments, celebrate life, or make every day a little
+        brighter, we&apos;re here to turn your vision into reality. It&apos;s
+        all about bringing joy, embracing the fun, and never being just another
+        face in the crowd. Let&apos;s make your memories as magical as the
+        moments themselves!
       </span>
     </div>
   );
